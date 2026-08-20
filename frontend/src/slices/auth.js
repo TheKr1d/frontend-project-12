1b2776a0bebb7a1c1297b9f4c58b8842a79d6710
Vlad <epifanov.vv.spb@gmail.com>
2026-08-20 17:38:33 +0300

    fix render.com

--- frontend/src/slices/auth.js
+++ frontend/src/slices/auth.js
@@ -19,7 +19,6 @@ export const loginUser = createAsyncThunk(
     const loadId = notificationService.loading('notifications.loginUserLoad');
     try {
       const response = await axios.post(routes.login(), { username, password });
-      console.log('📦 Ответ от бэкенда:', response.data); // 👈 ДОБАВЬТЕ ЭТО
       addToken({
         username: response.data.username,
         token: response.data.token,
