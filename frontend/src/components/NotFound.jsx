15a41f90d65918da92168c140cdd5f033cd0a0c5
Vlad <epifanov.vv.spb@gmail.com>
2026-08-24 20:04:31 +0300

    add i18n in file NotFound

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,14 +1,18 @@
 import { Col, Container, Row } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
-const NotFound = () => (
-  <Container className="min-vh-100 d-flex align-items-center justify-content-center">
-    <Row>
-      <Col>
-        <h1 className="display-1 text-muted text-center">404</h1>
-        <p className="text-center text-secondary">Страница не найдена</p>
-      </Col>
-    </Row>
-  </Container>
-);
+const NotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <Container className="min-vh-100 d-flex align-items-center justify-content-center">
+      <Row>
+        <Col>
+          <h1 className="display-1 text-muted text-center">404</h1>
+          <p className="text-center text-secondary">{t('fieldErrors.notFound')}</p>
+        </Col>
+      </Row>
+    </Container>
+  )
+};
 
 export default NotFound;
